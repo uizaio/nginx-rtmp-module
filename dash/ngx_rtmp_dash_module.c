521c31fdb9b8c3e1b9c8c62e2a8a240e893c1c65
ducla <ducla@uiza.io>
2019-09-07 18:28:49 +0700

    FIX:can not create playlist

--- dash/ngx_rtmp_dash_module.c
+++ dash/ngx_rtmp_dash_module.c
@@ -871,9 +871,10 @@ ngx_rtmp_dash_publish(ngx_rtmp_session_t *s, ngx_rtmp_publish_t *v)
         }
     }
 
-    ctx->id = 0;
-    ngx_log_error(NGX_LOG_ERR, s->connection->log, 0,"dash - ducla");
+    ctx->id = 0;    
     if (ngx_strstr(v->name, "..")) {
+        ngx_log_error(NGX_LOG_ERR, s->connection->log, 0,
+                      "ducla: bad stream name: '%s'", v->name);
         ngx_log_error(NGX_LOG_ERR, s->connection->log, 0,
                       "dash: bad stream name: '%s'", v->name);
         return NGX_ERROR;
