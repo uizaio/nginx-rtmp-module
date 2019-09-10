0f90a7ccb2470a3e3f9d1f66df41b52f4fbd040c
ducla <ducla@uiza.io>
2019-09-10 14:56:19 +0700

    FIX: playlist is not updated when has new data

--- hls/ngx_rtmp_hls_module.c
+++ hls/ngx_rtmp_hls_module.c
@@ -948,7 +948,8 @@ ngx_rtmp_hls_open_fragment(ngx_rtmp_session_t *s, uint64_t ts,
     }
 
     ctx->opened = 1;
-
+    ngx_log_error(NGX_LOG_ERR, s->connection->log, ngx_errno,
+                          "hls: nfrags %d", ctx->nfrags);
     f = ngx_rtmp_hls_get_frag(s, ctx->nfrags);
 
     ngx_memzero(f, sizeof(*f));
@@ -1464,9 +1465,7 @@ ngx_rtmp_hls_publish(ngx_rtmp_session_t *s, ngx_rtmp_publish_t *v)
 
     if (hacf->continuous) {
         ngx_rtmp_hls_restore_stream(s);
-    }
-    ngx_log_error(NGX_LOG_ERR, s->connection->log, ngx_errno,
-                          "hls: nfrags %d", ctx->nfrags);
+    }    
 next:
     return next_publish(s, v);
 }
