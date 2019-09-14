df6de1d868331f713604659a8242f3e881d194af
ducla <ducla@uiza.io>
2019-09-15 00:59:23 +0700

    write m4s data

--- fmp4/ngx_rtmp_fmp4.h
+++ fmp4/ngx_rtmp_fmp4.h
@@ -68,7 +68,9 @@ ngx_int_t ngx_rtmp_fmp4_write_traf(ngx_buf_t *b, uint32_t earliest_pres_time,
 ngx_int_t ngx_rtmp_fmp4_write_tfhd(ngx_buf_t *b, uint32_t track_id);
 ngx_int_t ngx_rtmp_fmp4_write_tfdt(ngx_buf_t *b, uint32_t earliest_pres_time);
 ngx_int_t ngx_rtmp_fmp4_write_trun(ngx_buf_t *b, uint32_t sample_count,
-    ngx_rtmp_fmp4_sample_t *samples, ngx_uint_t sample_mask, u_char *moof_pos);
+    ngx_rtmp_fmp4_sample_t *samples, ngx_uint_t sample_mask, u_char *moof_pos, 
+    uint32_t next_sample_count,
+    ngx_rtmp_fmp4_sample_t *next_samples, ngx_uint_t next_sample_mask, uint32_t isVideo)
 
 
 #endif /* _NGX_RTMP_FMP4_H_INCLUDED_ */
