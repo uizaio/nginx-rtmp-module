0ca18f9420c4857808c809ca07b2b43d7fe701fd
ducla <ducla@uiza.io>
2019-09-15 12:33:51 +0700

    ADD: playlist

--- fmp4/ngx_rtmp_fmp4_module.c
+++ fmp4/ngx_rtmp_fmp4_module.c
@@ -542,6 +542,9 @@ ngx_rtmp_fmp4_write_playlist(ngx_rtmp_session_t *s){
     if (ctx->id == 0) {
         ngx_rtmp_fmp4_write_init(s);
     }
+    ngx_log_error(NGX_LOG_ERR, s->connection->log, ngx_errno,
+                      "fmp4: open %s",
+                      ctx->playlist_bak.data);
     fd = ngx_open_file(ctx->playlist_bak.data, NGX_FILE_WRONLY,
                        NGX_FILE_TRUNCATE, NGX_FILE_DEFAULT_ACCESS);
     if (fd == NGX_INVALID_FILE) {
