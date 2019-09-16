0738af330e60e302256a9d3ffd6c561e0e638bc8
ducla <ducla@uiza.io>
2019-09-17 00:22:04 +0700

    zero trun

--- fmp4/ngx_rtmp_fmp4.c
+++ fmp4/ngx_rtmp_fmp4.c
@@ -974,14 +974,12 @@ ngx_rtmp_fmp4_write_trun(ngx_buf_t *b, uint32_t sample_count,
     ngx_rtmp_fmp4_field_32(b, flags);
     ngx_rtmp_fmp4_field_32(b, sample_count);
     ngx_rtmp_fmp4_field_32(b, offset);
-    truns->last_video_trun += 20;
-    truns->last_audio_trun += 20;
     ngx_log_error(NGX_LOG_INFO, s->connection->log, 0,
                           "fmp42: trun %d", truns->last_video_trun);
     if(isVideo == 0){
-        truns->last_video_trun += (sample_count -1) * nitems * 4;
+        truns->last_video_trun += (sample_count -1) * nitems * 4 + 20;
     }else{
-        truns->last_audio_trun += (sample_count -1) * nitems * 4;
+        truns->last_audio_trun += (sample_count -1) * nitems * 4 + 20;
     }
     ngx_log_error(NGX_LOG_INFO, s->connection->log, 0,
                           "fmp42: trun %d", truns->last_video_trun); 
