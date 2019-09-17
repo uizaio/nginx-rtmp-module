fc24efcb2112a3672c5022bcd352140f631b1213
ducla <ducla@uiza.io>
2019-09-17 22:53:27 +0700

    fix: zero duration sample in trun atom

--- fmp4/ngx_rtmp_fmp4_module.c
+++ fmp4/ngx_rtmp_fmp4_module.c
@@ -414,6 +414,8 @@ ngx_rtmp_fmp4_write_data(ngx_rtmp_session_t *s,  ngx_rtmp_fmp4_track_t *vt,  ngx
 
     ctx = ngx_rtmp_get_module_ctx(s, ngx_rtmp_fmp4_module);    
     *ngx_sprintf(ctx->stream.data + ctx->stream.len, "%uD.m4s", ctx->id) = 0;
+    ctx->last_chunk_file.len = ngx_strlen(ctx->stream.len);
+    ctx->last_chunk_file.data = ngx_palloc(s->connection->pool, ctx->stream.len + 1);
     *ngx_cpymem(ctx->last_chunk_file.data, ctx->stream.data, ctx->stream.len) = 0;
     ngx_log_error(NGX_LOG_ERR, s->connection->log, 0,
                       "fmp4: create file %s", ctx->stream.data);
