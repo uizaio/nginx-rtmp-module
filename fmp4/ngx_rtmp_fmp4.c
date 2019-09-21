2166aa2e3a437780f1fe3b7bf525e256b7dc96d9
ducla <ducla@uiza.io>
2019-09-21 23:06:17 +0700

    FIX: wrong NAL unit size

--- fmp4/ngx_rtmp_fmp4.c
+++ fmp4/ngx_rtmp_fmp4.c
@@ -967,7 +967,7 @@ ngx_rtmp_fmp4_write_trun(ngx_buf_t *b, uint32_t sample_count,
             next_nitems++;
         }
         //size_of_sample + 48  + (next_traf[8] + tfhd[28] + tfdt[20] + trun[20])        
-        offset = (pos - moof_pos) + (sample_count * nitems * 4)(next_sample_count * next_nitems * 4) + 88;               
+        offset = (pos - moof_pos) + (sample_count * nitems * 4) + (next_sample_count * next_nitems * 4) + 88;               
     }else{
         // = size_of_sample + 
         offset = (pos - moof_pos) + 20 + (sample_count * nitems * 4) + 8;        
