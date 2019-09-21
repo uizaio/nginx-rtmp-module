69b40994ba6a562260e75f8528e1784dcd8b4fef
ducla <ducla@uiza.io>
2019-09-21 21:59:07 +0700

    FIX: wrong NAL unit size

--- fmp4/ngx_rtmp_fmp4_module.c
+++ fmp4/ngx_rtmp_fmp4_module.c
@@ -861,7 +861,7 @@ ngx_rtmp_fmp4_append(ngx_rtmp_session_t *s, ngx_chain_t *in,
         smpl = &t->samples[t->sample_count];
 
         smpl->delay = delay;
-        smpl->size = (uint32_t) size + (isVideo == 0 ? 4 : 0);//we add 4byte nal in video sample
+        smpl->size = (uint32_t) size + (isVideo == 1 ? 4 : 0);//we add 4byte nal in video sample
         ngx_log_error(NGX_LOG_ERR, s->connection->log, 0,
                           "fmp4 - 837: sample size %d: %d",isVideo, size);
         smpl->duration = t->codec->duration;
