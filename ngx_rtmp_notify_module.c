ea8cea6f203e5e86caf76928ed9597eda0ce02d6
ducla <ducla@uiza.io>
2019-09-27 15:55:33 +0700

    parse header of response

--- ngx_rtmp_notify_module.c
+++ ngx_rtmp_notify_module.c
@@ -1024,7 +1024,7 @@ ngx_rtmp_notify_parse_http_body(ngx_rtmp_session_t *s, ngx_chain_t *in, int cont
     u_char*  tmp_body;
     ngx_str_t body;    
     
-    tmp_body = ngx_pcalloc(s->connection->pool, sizeof(u_char) * content_length + 1);
+    tmp_body = ngx_pcalloc(s->connection->pool, sizeof(u_char) * content_length + 3);//extend for \n\r\n
     if(tmp_body == NULL){
         return body;
     }
