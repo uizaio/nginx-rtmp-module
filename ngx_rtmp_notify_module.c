4ec6b5c432b508593d418feb2925364602a8fd14
ducla <ducla@uiza.io>
2019-09-27 17:12:40 +0700

    parse header of response

--- ngx_rtmp_notify_module.c
+++ ngx_rtmp_notify_module.c
@@ -1074,8 +1074,8 @@ ngx_rtmp_notify_parse_http_body(ngx_rtmp_session_t *s, ngx_chain_t *in, int cont
             }
         }        
         //FIXME: get to end or end - 1?                   
-//        body.data = ngx_pcalloc(s->connection->pool, sizeof(u_char) * (end - begin + 1));       
-        body.data = malloc(sizeof(u_char) * (end - begin + 1));
+        body.data = ngx_pcalloc(s->connection->pool, sizeof(u_char) * (end - begin + 1));       
+//        body.data = malloc(sizeof(u_char) * (end - begin + 1));
         if(body.data == NULL){
             return body;
         }              
@@ -1085,8 +1085,7 @@ ngx_rtmp_notify_parse_http_body(ngx_rtmp_session_t *s, ngx_chain_t *in, int cont
             j++;
         }        
     }    
-    free(tmp_body);
-    *(body.data + j + 1) = '\0';
+    free(tmp_body);    
     ngx_log_error(NGX_LOG_INFO, s->connection->log, 0,
                           "notify-1088: %s", body.data);    
     return body;
@@ -1256,7 +1255,8 @@ ngx_rtmp_notify_publish_handle(ngx_rtmp_session_t *s,
             }                        
         }
         if(body.data != NULL){
-            free(body.data);
+//            free(body.data);
+            ngx_pfree(s->connection->pool, body.data);
         }        
         goto next;
     }
