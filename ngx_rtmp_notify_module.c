04d85048d4d885eb581701483c5c199a5a2c97f7
ducla <ducla@uiza.io>
2019-09-27 13:23:06 +0700

    change on-the-fly playlist and stream path

--- ngx_rtmp_notify_module.c
+++ ngx_rtmp_notify_module.c
@@ -1145,7 +1145,7 @@ ngx_rtmp_notify_publish_handle(ngx_rtmp_session_t *s,
         ngx_rtmp_notify_parse_http_header(s, in, &location, name,
                                            sizeof(name) - 1);
         ngx_log_error(NGX_LOG_INFO, s->connection->log, 0,
-                      "notify-1145:'%s'", name);
+                      "notify-1145:'%s'", *name);
         body = ngx_rtmp_notify_parse_http_body(s, in);                
         if(body.len > 0){                        
             ctx = ngx_rtmp_get_module_ctx(s, ngx_rtmp_hls_module);   
