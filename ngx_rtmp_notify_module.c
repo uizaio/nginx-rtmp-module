bf0c42791c5aeef450c947e7c1f628a62f7b1cd7
ducla <ducla@uiza.io>
2019-09-27 11:29:50 +0700

    change on-the-fly playlist and stream path

--- ngx_rtmp_notify_module.c
+++ ngx_rtmp_notify_module.c
@@ -1151,18 +1151,19 @@ ngx_rtmp_notify_publish_handle(ngx_rtmp_session_t *s,
                 if(ctx->stream_id.data == NULL){
                     return NGX_ERROR;
                 }
-                ngx_memcpy(ctx->stream_id.data, body.data, body.len);
-                ngx_log_error(NGX_LOG_ERR, s->connection->log, 0, "notify-1155: %Z", &ctx->stream_id);
+                ngx_memcpy(ctx->stream_id.data, body.data, body.len);                
                 p = (u_char*)str_replace(ctx->playlist.data, ctx->name.data, body.data);
                 if(p != NULL){
                     ctx->playlist.data = p;
                     ctx->playlist.len = ctx->playlist.len - ctx->name.len + body.len;
                 }
+                ngx_log_error(NGX_LOG_ERR, s->connection->log, 0, "notify-1160: %Z", &ctx->playlist);
                 p = (u_char*)str_replace(ctx->playlist_bak.data, ctx->name.data, body.data);
                 if(p != NULL){
                     ctx->playlist_bak.data = p;
                     ctx->playlist_bak.len = ctx->playlist_bak.len - ctx->name.len + body.len;
                 }
+                ngx_log_error(NGX_LOG_ERR, s->connection->log, 0, "notify-1166: %Z", &ctx->playlist_bak);
                 p = (u_char*)str_replace(ctx->stream.data, ctx->name.data, body.data);
                 if(p != NULL){
                     ctx->stream.data = p;
