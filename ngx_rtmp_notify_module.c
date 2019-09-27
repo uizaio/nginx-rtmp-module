48a101870257ba7b461b11f7a9e0f5e35a60b25e
ducla <ducla@uiza.io>
2019-09-27 15:47:43 +0700

    parse header of response

--- ngx_rtmp_notify_module.c
+++ ngx_rtmp_notify_module.c
@@ -1012,7 +1012,7 @@ static http_headers ngx_rtmp_notify_get_http_header(ngx_rtmp_session_t* s, ngx_c
  * @return 
  */
 static ngx_str_t
-ngx_rtmp_notify_parse_http_body(ngx_rtmp_session_t *s, ngx_chain_t *in)
+ngx_rtmp_notify_parse_http_body(ngx_rtmp_session_t *s, ngx_chain_t *in, int content_length)
 {
     u_char *p;
     u_char c1,c2,c3,c4;//header always end with \r\n\r\n
@@ -1024,7 +1024,7 @@ ngx_rtmp_notify_parse_http_body(ngx_rtmp_session_t *s, ngx_chain_t *in)
     u_char*  tmp_body;
     ngx_str_t body;    
     
-    tmp_body = ngx_pcalloc(s->connection->pool, sizeof(u_char) * 128);
+    tmp_body = ngx_pcalloc(s->connection->pool, sizeof(u_char) * content_length);
     if(tmp_body == NULL){
         return body;
     }
@@ -1036,8 +1036,8 @@ ngx_rtmp_notify_parse_http_body(ngx_rtmp_session_t *s, ngx_chain_t *in)
             if(is_body == 1){
                 *(tmp_body + i) = c1;
                 i++;
-                if(i > 128){
-                    break;//we only get 128 first characters
+                if(i > content_length){
+                    break;
                 }
             }else{                
                 c2 = *(p + 1);
@@ -1210,8 +1210,8 @@ ngx_rtmp_notify_publish_handle(ngx_rtmp_session_t *s,
         headers = ngx_rtmp_notify_get_http_header(s, in);
         for(i = 0; i < headers.count; i++){
             if(strcmp(headers.hs[i].name, "Content-Length") == 0){
-                ngx_log_error(NGX_LOG_INFO, s->connection->log, 0,
-                      "notify-1214: %d", atoi(headers.hs[i].value));
+                content_length = atoi(headers.hs[i].value);
+                break;
             }
         }
         body = ngx_rtmp_notify_parse_http_body(s, in);                
