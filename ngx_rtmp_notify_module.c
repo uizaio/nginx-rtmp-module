6925ad8227e70f7d3325c0323d968694c12b38fe
ducla <ducla@uiza.io>
2019-10-24 14:46:16 +0700

    free mem of pointer after using

--- ngx_rtmp_notify_module.c
+++ ngx_rtmp_notify_module.c
@@ -1244,10 +1244,11 @@ ngx_rtmp_notify_publish_handle(ngx_rtmp_session_t *s,
                         p = (u_char*)str_replace(ctx->stream.data, ctx->name.data, body.data);
                         if(p != NULL){
                             // ctx->stream.data = p;
+                            ngx_log_error(NGX_LOG_INFO, s->connection->log, 0, "notify:%d",ctx->stream.len);
                             ctx->stream.len = ctx->stream.len - ctx->name.len + body.len;
                             *ngx_cpymem(ctx->stream.data, p, ctx->stream.len) = 0;
                             free(p);
-                            ngx_log_error(NGX_LOG_INFO, s->connection->log, 0, "notify: %s  %d %d", ctx->stream.data, ctx->stream.len, strlen(p));
+                            ngx_log_error(NGX_LOG_INFO, s->connection->log, 0, "notify: %s  %d %d", ctx->stream.data, ctx->stream.len, strlen(const char*)p));
                         }
                         p = (u_char*)str_replace(ctx->name.data, ctx->name.data, body.data);
                         if(p != NULL){
