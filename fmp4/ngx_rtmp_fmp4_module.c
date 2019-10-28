8d8970df86252e8888915c863ac8894a5454b2c3
ducla <ducla@uiza.io>
2019-10-28 15:46:51 +0700

    fix nal size

--- fmp4/ngx_rtmp_fmp4_module.c
+++ fmp4/ngx_rtmp_fmp4_module.c
@@ -793,7 +793,10 @@ ngx_rtmp_fmp4_append(ngx_rtmp_session_t *s, ngx_chain_t *in,
     uint32_t                duration;    
 
     static u_char           buffer[NGX_RTMP_FMP4_BUFSIZE];
-    p = buffer + (isVideo == 1 ? 4 : 0); //save 4 byte for nal
+    //=======ducla=====
+    // p = buffer + (isVideo == 1 ? 4 : 0); //save 4 byte for nal
+    //=======
+    p = buffer;
     size = 0;    
 
     ctx = ngx_rtmp_get_module_ctx(s, ngx_rtmp_fmp4_module);
@@ -839,18 +842,20 @@ ngx_rtmp_fmp4_append(ngx_rtmp_session_t *s, ngx_chain_t *in,
     // }    
     t->latest_pres_time = timestamp;    
     if (t->sample_count < NGX_RTMP_FMP4_MAX_SAMPLES) {
-        if(isVideo == 1){
-            //write data to raw file
-            //we need to insert 4byte nal component in here for video
-            // ngx_log_error(NGX_LOG_ERR, s->connection->log, 0,
-            //               "fmp4 - 837: sample size: %d", size);
+        //======ducla======
+        // if(isVideo == 1){
+        //     //write data to raw file
+        //     //we need to insert 4byte nal component in here for video
+        //     // ngx_log_error(NGX_LOG_ERR, s->connection->log, 0,
+        //     //               "fmp4 - 837: sample size: %d", size);
             
-            buffer[0] = ((uint32_t) size >> 24) & 0xFF;
-            buffer[1] = ((uint32_t) size >> 16) & 0xFF;
-            buffer[2] = ((uint32_t) size >> 8) & 0xFF;
-            buffer[3] = (uint32_t) size & 0xFF;
-        }        
-        if (ngx_write_fd(t->fd, buffer, size + 4) == NGX_ERROR) {
+        //     buffer[0] = ((uint32_t) size >> 24) & 0xFF;
+        //     buffer[1] = ((uint32_t) size >> 16) & 0xFF;
+        //     buffer[2] = ((uint32_t) size >> 8) & 0xFF;
+        //     buffer[3] = (uint32_t) size & 0xFF;
+        // }
+        //==========        
+        if (ngx_write_fd(t->fd, buffer, size /*+ 4*/) == NGX_ERROR) {
             ngx_log_error(NGX_LOG_ERR, s->connection->log, ngx_errno,
                           "fmp4: " ngx_write_fd_n " failed");
             return NGX_ERROR;
@@ -859,7 +864,7 @@ ngx_rtmp_fmp4_append(ngx_rtmp_session_t *s, ngx_chain_t *in,
         smpl = &t->samples[t->sample_count];
 
         smpl->delay = delay;
-        smpl->size = (uint32_t) size + (isVideo == 1 ? 4 : 0);//we add 4byte nal in video sample        
+        smpl->size = (uint32_t) size /*+ (isVideo == 1 ? 4 : 0)*/;//we add 4byte nal in video sample        
         smpl->duration = t->codec->duration;
         smpl->timestamp = timestamp;
         smpl->key = (key ? 1 : 0);
