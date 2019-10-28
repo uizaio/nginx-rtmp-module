7a8932ae4dbca33f6af98498fb973ea07c666238
ducla <ducla@uiza.io>
2019-10-28 18:08:20 +0700

    fix nal size

--- fmp4/ngx_rtmp_fmp4_module.c
+++ fmp4/ngx_rtmp_fmp4_module.c
@@ -223,8 +223,9 @@ ngx_rtmp_fmp4_video(ngx_rtmp_session_t *s, ngx_rtmp_header_t *h,
     ctx->has_video = 1;
     in->buf->pos += 5;
     ctx->video.codec = codec_ctx;
-    ngx_log_error(NGX_LOG_ERR, s->connection->log, 0,
-                       "fmp4: nal byte = %d", codec_ctx->avc_nal_bytes);
+    u_char pp[4];
+    // ngx_log_error(NGX_LOG_ERR, s->connection->log, 0,
+    //                    "fmp4: nal byte = %d", codec_ctx->avc_nal_bytes);
     return ngx_rtmp_fmp4_append(s, in, &ctx->video, ftype == 1, h->timestamp,
                                 delay, 1);
 }
@@ -844,19 +845,6 @@ ngx_rtmp_fmp4_append(ngx_rtmp_session_t *s, ngx_chain_t *in,
     // }    
     t->latest_pres_time = timestamp;    
     if (t->sample_count < NGX_RTMP_FMP4_MAX_SAMPLES) {
-        //======ducla======
-        // if(isVideo == 1){
-        //     //write data to raw file
-        //     //we need to insert 4byte nal component in here for video
-        //     // ngx_log_error(NGX_LOG_ERR, s->connection->log, 0,
-        //     //               "fmp4 - 837: sample size: %d", size);
-            
-        //     buffer[0] = ((uint32_t) size >> 24) & 0xFF;
-        //     buffer[1] = ((uint32_t) size >> 16) & 0xFF;
-        //     buffer[2] = ((uint32_t) size >> 8) & 0xFF;
-        //     buffer[3] = (uint32_t) size & 0xFF;
-        // }
-        //==========        
         if (ngx_write_fd(t->fd, buffer, size /*+ 4*/) == NGX_ERROR) {
             ngx_log_error(NGX_LOG_ERR, s->connection->log, ngx_errno,
                           "fmp4: " ngx_write_fd_n " failed");
