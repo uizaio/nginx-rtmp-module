9ca5dbcb050e4d534c321f15579d8bb4d6f3bfbb
ducla <ducla@uiza.io>
2019-12-02 22:29:38 +0700

    replace str_replace function

--- fmp4/ngx_rtmp_fmp4_module.c
+++ fmp4/ngx_rtmp_fmp4_module.c
@@ -436,12 +436,12 @@ ngx_rtmp_fmp4_write_data(ngx_rtmp_session_t *s,  ngx_rtmp_fmp4_track_t *vt,  ngx
     u_char                          *pos, *pos1;
     ngx_rtmp_fmp4_last_sample_trun  *truns;
     uint64_t                        id;
-    ngx_rtmp_fmp4_app_conf_t        *facf;
+    // ngx_rtmp_fmp4_app_conf_t        *facf;
     ngx_rtmp_fmp4_frag_t            *f;
     
 
     ctx = ngx_rtmp_get_module_ctx(s, ngx_rtmp_fmp4_module); 
-    facf = ngx_rtmp_get_module_app_conf(s, ngx_rtmp_fmp4_module);
+    // facf = ngx_rtmp_get_module_app_conf(s, ngx_rtmp_fmp4_module);
     //we need to choose a naming for fragments
     //we collect it each current frag
     f = ngx_rtmp_fmp4_get_frag(s, ctx->nfrags);   
