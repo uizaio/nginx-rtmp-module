eaf71ed726f815e4ccc5adf0acb6282d038027d5
ducla <ducla@uiza.io>
2020-02-10 14:29:40 +0700

    debug 1

--- transcode/ngx_rtmp_transcode_module.c
+++ transcode/ngx_rtmp_transcode_module.c
@@ -242,6 +242,10 @@ ngx_rtmp_transcode_video(ngx_rtmp_session_t *s, ngx_rtmp_header_t *h,
 static ngx_int_t
 ngx_rtmp_transcode_postconfiguration(ngx_conf_t *cf)
 {
+    ngx_rtmp_handler_pt        *h;
+    ngx_rtmp_core_main_conf_t  *cmcf;
+
+    cmcf = ngx_rtmp_conf_get_module_main_conf(cf, ngx_rtmp_core_module);
     h = ngx_array_push(&cmcf->events[NGX_RTMP_MSG_VIDEO]);
     *h = ngx_rtmp_transcode_video;
 
