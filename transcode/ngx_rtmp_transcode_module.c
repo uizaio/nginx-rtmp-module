83f5707fed9336381b1f079c60fcaf5121ca5879
ducla <ducla@uiza.io>
2020-02-10 14:24:31 +0700

    debug 1

--- transcode/ngx_rtmp_transcode_module.c
+++ transcode/ngx_rtmp_transcode_module.c
@@ -223,6 +223,18 @@ ngx_rtmp_transcode_close_stream(ngx_rtmp_session_t *s, ngx_rtmp_close_stream_t *
 static ngx_int_t
 ngx_rtmp_transcode_stream_begin(ngx_rtmp_session_t *s, ngx_rtmp_stream_begin_t *v)
 {
+    ngx_rtmp_codec_ctx_t    *codec_ctx;
+    ngx_uint_t              video_rate;
+
+    codec_ctx = ngx_rtmp_get_module_ctx(s, ngx_rtmp_codec_module);
+    if(codec_ctx != NULL){
+        video_rate = codec_ctx->video_data_rate;
+        ngx_log_error(NGX_LOG_ERR, s->connection->log, ngx_errno,
+                      "transcode: video rate: %d", video_rate);
+    }else{
+        ngx_log_error(NGX_LOG_ERR, s->connection->log, ngx_errno,
+                      "transcode: No video rate");
+    }
     return next_stream_begin(s, v);
 }
 
