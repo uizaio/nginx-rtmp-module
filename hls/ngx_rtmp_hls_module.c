e899ce777644217fde92f1a96fd3d2d3d4509e17
ducla <ducla@uiza.io>
2020-02-10 11:57:07 +0700

    resolve conflict vars

--- hls/ngx_rtmp_hls_module.c
+++ hls/ngx_rtmp_hls_module.c
@@ -34,54 +34,6 @@ static ngx_int_t ngx_rtmp_hls_ensure_directory(ngx_rtmp_session_t *s,
 #define NGX_RTMP_HLS_DIR_ACCESS         0744
 
 
-typedef struct {
-    uint64_t                            id;
-    uint64_t                            key_id;
-    double                              duration;
-    unsigned                            active:1;
-    unsigned                            discont:1; /* before */
-} ngx_rtmp_hls_frag_t;
-
-
-typedef struct {
-    ngx_str_t                           suffix;
-    ngx_array_t                         args;
-} ngx_rtmp_hls_variant_t;
-
-
-typedef struct {
-    unsigned                            opened:1;
-
-    ngx_rtmp_mpegts_file_t              file;
-
-    ngx_str_t                           playlist;
-    ngx_str_t                           playlist_bak;
-    ngx_str_t                           var_playlist;
-    ngx_str_t                           var_playlist_bak;
-    ngx_str_t                           stream;
-    ngx_str_t                           keyfile;
-    ngx_str_t                           name;
-    u_char                              key[16];
-
-    uint64_t                            frag; //point to the first fragment in playlist
-    uint64_t                            frag_ts;
-    uint64_t                            key_id;
-    ngx_uint_t                          nfrags; //pont to the last fragment in playlist
-    ngx_rtmp_hls_frag_t                *frags; /* circular 2 * winfrags + 1 */
-
-    ngx_uint_t                          audio_cc;
-    ngx_uint_t                          video_cc;
-    ngx_uint_t                          key_frags;
-
-    uint64_t                            aframe_base;
-    uint64_t                            aframe_num;
-
-    ngx_buf_t                          *aframe;
-    uint64_t                            aframe_pts;
-
-    ngx_rtmp_hls_variant_t             *var;
-} ngx_rtmp_hls_ctx_t;
-
 
 typedef struct {
     ngx_str_t                           path;
