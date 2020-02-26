46ba4e8174831009dbf0e20f2648604b9baa262c
ducla <ducla@uiza.io>
2020-02-26 14:55:22 +0700

    read output params

--- transcode/ngx_rtmp_transcode_module.c
+++ transcode/ngx_rtmp_transcode_module.c
@@ -6,7 +6,7 @@
 #include <ngx_rtmp.h>
 #include <ngx_rtmp_codec_module.h>
 #include <ngx_rtmp_cmd_module.h>
-#include <ngx_rtmp_transcode_module.h>
+#include "ngx_rtmp_transcode_module.h"
 
 static ngx_rtmp_publish_pt              next_publish;
 static ngx_rtmp_close_stream_pt         next_close_stream;
